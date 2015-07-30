80323c0c4e5321700b2168bd0e80f2bcc6a94eaa
Ma Eikei <yma@asratec.co.jp>
2015-07-30 11:23:33 +0900

    new ux game

---
+++ installv0.95.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+wget --no-check-certificate https://asratec.github.io/VSidoConn4Edison/Binary/v0.95/VSidoConn4Edison.tar.gz -O VSidoConn4Edison.tar.gz 
+mkdir -p /home/sysroot/
+tar -xzvf VSidoConn4Edison.tar.gz -C /home/sysroot/
+make -C /home/sysroot/usr/share/OpkgEnv
+make -C /home/sysroot/usr/share/OpkgEnv install
+make -C /home/sysroot/usr/share/Config
+sync
+sync
+
+
+
